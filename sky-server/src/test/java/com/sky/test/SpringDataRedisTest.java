976bb0a8f562932d09a77f43aa77114a5fb991da
Yilin Sun <sun.sylvan@outlook.com>
2024-06-30 11:05:31 +0800

    feat: Redis test cases

--- sky-server/src/test/java/com/sky/test/SpringDataRedisTest.java
+++ sky-server/src/test/java/com/sky/test/SpringDataRedisTest.java
@@ -3,8 +3,11 @@ package com.sky.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.core.*;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -33,5 +36,86 @@ public class SpringDataRedisTest {
         redisTemplate.opsForValue().setIfAbsent("lock","2");
         String value = (String) redisTemplate.opsForValue().get("lock");
         System.out.println(value);
+    }
+
+    @Test
+    public void testHash(){
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        hashOperations.put("100","name", "tom");
+        hashOperations.put("100","age", "20");
+
+        String name = (String) hashOperations.get("100", "name");
+        Set keys = hashOperations.keys("100");
+        List values = hashOperations.values("100");
+
+        System.out.println(name);
+        System.out.println(keys);
+        System.out.println(values);
+
+        hashOperations.delete("100","age");
+    }
+
+    @Test
+    public void testList(){
+        ListOperations listOperations = redisTemplate.opsForList();
+        listOperations.leftPushAll("mylist", "a", "b", "c");
+        listOperations.leftPush("mylist","d");
+        List mylist = listOperations.range("mylist", 0, -1);
+        System.out.println(mylist);
+
+        listOperations.rightPop("mylist");
+        Long size = listOperations.size("mylist");
+        System.out.println(size);
+    }
+
+    @Test
+    public void testSet(){
+        SetOperations setOperations = redisTemplate.opsForSet();
+        setOperations.add("set1", "a", "b", "c", "d");
+        setOperations.add("set2", "a", "b", "x", "y");
+
+        Set members = setOperations.members("set1");
+        Long size = setOperations.size("set1");
+
+        System.out.println(members);
+        System.out.println(size);
+
+        Set intersect = setOperations.intersect("set1", "set2");
+        Set union = setOperations.union("set1", "set2");
+        System.out.println(intersect);
+        System.out.println(union);
+
+        setOperations.remove("set1", "a","b");
+    }
+
+    @Test
+    public void testZSet(){
+        ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+        zSetOperations.add("zset1", "a", 10);
+        zSetOperations.add("zset1", "b", 12);
+        zSetOperations.add("zset1", "c", 9);
+
+        Set zset1 = zSetOperations.range("zset1", 0, -1);
+        System.out.println(zset1);
+
+        zSetOperations.incrementScore("zset1", "c", 10);
+        Set zset2 = zSetOperations.range("zset1", 0, -1);
+        System.out.println(zset2);
+    }
+    
+    @Test
+    public void testCommon(){
+        Set keys = redisTemplate.keys("*");
+        System.out.println(keys);
+
+        Boolean name = redisTemplate.hasKey("name");
+        Boolean set1 = redisTemplate.hasKey("set1");
+
+        for (Object key : keys) {
+            DataType type = redisTemplate.type(key);
+            System.out.println(type.name());
+        }
+
+        redisTemplate.delete("mylist");
     }
 }
